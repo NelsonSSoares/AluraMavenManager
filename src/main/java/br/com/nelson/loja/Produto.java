8c076d9314ebea767f067f927fa09e8c9a6afdf6
Nelson <punkpunkada@gmail.com>
2022-07-13 19:37:28 -0300

    Configurando build e Novos comandos

--- src/main/java/br/com/nelson/loja/Produto.java
+++ src/main/java/br/com/nelson/loja/Produto.java
@@ -1,5 +1,15 @@
 package br.com.nelson.loja;
 
 public class Produto {
-
+	/*
+	 * CRIAR VARIAVEL DE AMBIENTE OU INCLUIR NO XML
+	 * MAVEN COMANDS
+	 * mvn compile // compila o codigo
+	 * mvn clean // limpa repositorio
+	 * mvn test // teste automatizado
+	 * mvn package // gera o jar
+	 * mvn intall // gera o jar e coloca o repositorio local
+	 * mvn deploy // gera o jar e coloca em repositorio remoto configurando no xml
+	 * 
+	 */
 }
